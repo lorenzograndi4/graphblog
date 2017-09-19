1772b11c3991082b3ff594765ddf90b3272d0506
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-09-19 16:45:25 +0200

    add limits

--- app/graphql/graphblog_schema.rb
+++ app/graphql/graphblog_schema.rb
@@ -1,4 +1,8 @@
 GraphblogSchema = GraphQL::Schema.define do
+  max_depth 4 # adjust as required
+  use GraphQL::Batch
+  enable_preloading
+  
   mutation(Types::MutationType)
   query(Types::QueryType)
 end
--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -5,4 +5,8 @@ Types::PostType = GraphQL::ObjectType.define do
   field :body, types.String
   field :comments, -> { !types[Types::CommentType] }
   field :postedBy, -> { Types::UserType }, property: :user
+  # alternatively
+  # field :postedBy, Types::UserType do
+  #   resolve -> (obj, args, context) { RecordLoader.for(User).load(obj.user_id) }
+  # end
 end
